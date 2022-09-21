7d5846d013807f6d9b8c92329515342c625cf2fe
f1le47 <ospischeff@gmail.com>
2022-09-22 00:28:26 +0300

    removed redundant files

---
+++ src/components/Main/Header/Header.tsx
@@ -0,0 +1,9 @@
+import s from './Header.module.scss'
+
+const Header = () => {
+  return (
+    <div className={s.header}></div>
+  )
+}
+
+export default Header
---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,12 @@
+import Header from '../../components/Main/Header/Header'
+import s from './Main.module.scss'
+
+const Main = () => {
+  return (
+    <div className={s.main}>
+      <Header />
+    </div>
+  )
+}
+
+export default Main
